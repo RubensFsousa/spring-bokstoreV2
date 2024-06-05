1132956b4005d7a00b68f47d85e5cfc159b92782
Rubens.F <xansxxl@gmail.com>
2024-06-05 20:37:03 -0300

    feat: add base docs asbtraction

--- src/main/java/com/study/spring/base/authentication/api/controllers/AuthController.java
+++ src/main/java/com/study/spring/base/authentication/api/controllers/AuthController.java
@@ -2,24 +2,24 @@ package com.study.spring.base.authentication.api.controllers;
 
 import com.study.spring.base.authentication.api.controllers.DTOs.LoginRequestDTO;
 import com.study.spring.base.authentication.api.controllers.DTOs.LoginResponseDTO;
+import com.study.spring.base.authentication.api.controllers.annotations.LoginEndPoint;
 import com.study.spring.base.authentication.domain.services.AuthService;
+import com.study.spring.base.shared.annotations.ApiController;
+import com.study.spring.base.shared.annotations.OpenApiController;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("auth")
 @AllArgsConstructor
+@OpenApiController(name = "Auth")
+@ApiController(path = "/auth")
 public class AuthController {
 
     private final AuthService authService;
 
-    @PostMapping("/login")
+    @LoginEndPoint
     public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid LoginRequestDTO request) {
         var response = authService.authenticate(request);
         return new ResponseEntity<>(response, HttpStatus.OK);
---
+++ src/main/java/com/study/spring/base/shared/annotations/OpenApiResponse404.java
@@ -0,0 +1,18 @@
+package com.study.spring.base.shared.annotations;
+
+import com.study.spring.base.shared.models.DTOs.ProblemDTO;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+import java.lang.annotation.*;
+
+@ApiResponse(
+        responseCode = "404",
+        description = "Resource not found.",
+        content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProblemDTO.class))
+)
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface OpenApiResponse404 {
+}
